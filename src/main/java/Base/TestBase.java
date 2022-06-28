c0a2af4940e52075e3ea13e969b33db5313a6fed
aniket patil <shubhampatil3971@gmail.com>
2022-06-28 08:53:30 +0530

    first commit

---
+++ src/main/java/Base/TestBase.java
@@ -0,0 +1,18 @@
+package Base;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class TestBase {
+	public static WebDriver driver;
+	public void Initalization() {
+		System.setProperty("webdriver.chrome.driver",	"chromedriver");
+		 driver	=	new	ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("https://kite.zerodha.com/");
+		driver.manage().timeouts().implicitlyWait(25,	TimeUnit.SECONDS);
+	}
+
+}
